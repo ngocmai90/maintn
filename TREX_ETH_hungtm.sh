6ee3c9045bf3589c3aaf8687b1901715a22a08f6
ngocmai90 <84979022+ngocmai90@users.noreply.github.com>
2021-06-17 20:44:23 +0700

    Update TREX_ETH_hungtm.sh

--- TREX_ETH_hungtm.sh
+++ TREX_ETH_hungtm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x6f0dcd0c1b3669d6448c1bd209c4d9c18a59bf5a -p x -w maintn
+./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x824887a52b5efd207643eef1d0cd9e570fec2a3 -p x -w hungnv
