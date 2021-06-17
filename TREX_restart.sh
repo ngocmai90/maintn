2db242544853983ddb304728926842606f263a6e
ngocmai90 <84979022+ngocmai90@users.noreply.github.com>
2021-06-17 22:17:43 +0700

    Delete TREX_restart.sh

--- TREX_restart.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-sudo ps -ef | grep 't-rex' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-sudo rm -rf /home/ubuntu/TREX/log.log && sudo nohup sh /home/ubuntu/TREX/TREX_ETH_maintn.sh >> /home/ubuntu/TREX/log.log 2>&1 &
